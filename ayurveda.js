53e8b6520ee08cd9e196214bf36f6535525bc4bf
skale0181 <skale0181@gmail.com>
2021-12-15 13:57:46 +0530

    small changes in ayurveda page

---
+++ ayurveda.js
@@ -0,0 +1,158 @@
+
+
+    // product data store in arr of object bellow
+      var productData = [
+    
+    {
+        img:"https://static.oxinis.com/healthmug/image/product/5317-2-200.jpg",
+        nam:"Dr. Loonawat Wartosin (3ml)",
+        detail:"Ayurvedic composition for elevated or hanging warts",
+        logo:"	https://static.oxinis.com/healthmug/image/asset/498-rm-150.png",
+        price:"80",
+        s1:"fas fa-star",
+        s2:"fas fa-star",
+        s3:"fas fa-star",
+        s4:"fas fa-star-half-alt",
+        s5:"far fa-star",
+    },
+    {
+        img:"https://static.oxinis.com/healthmug/image/product/5317-2-200.jpg",
+        nam:"Dr. Loonawat Wartosin (3ml)",
+        detail:"Ayurvedic composition for elevated or hanging warts",
+        logo:"	https://static.oxinis.com/healthmug/image/asset/498-rm-150.png",
+        price:"80",
+    },
+    {
+        img:"https://static.oxinis.com/healthmug/image/product/5317-2-200.jpg",
+        nam:"Dr. Loonawat Wartosin (3ml)",
+        detail:"Ayurvedic composition for elevated or hanging warts",
+        logo:"	https://static.oxinis.com/healthmug/image/asset/498-rm-150.png",
+        price:"80",
+    },
+    {
+        img:"https://static.oxinis.com/healthmug/image/product/5317-2-200.jpg",
+        nam:"Dr. Loonawat Wartosin (3ml)",
+        detail:"Ayurvedic composition for elevated or hanging warts",
+        logo:"	https://static.oxinis.com/healthmug/image/asset/498-rm-150.png",
+        price:"80",
+    },
+    {
+        img:"https://static.oxinis.com/healthmug/image/product/5317-2-200.jpg",
+        nam:"Dr. Loonawat Wartosin (3ml)",
+        detail:"Ayurvedic composition for elevated or hanging warts",
+        logo:"	https://static.oxinis.com/healthmug/image/asset/498-rm-150.png",
+        price:"80",
+    },
+    {
+        img:"https://static.oxinis.com/healthmug/image/product/5317-2-200.jpg",
+        nam:"Dr. Loonawat Wartosin (3ml)",
+        detail:"Ayurvedic composition for elevated or hanging warts",
+        logo:"	https://static.oxinis.com/healthmug/image/asset/498-rm-150.png",
+        price:"80",
+    },
+    {
+        img:"https://static.oxinis.com/healthmug/image/product/5317-2-200.jpg",
+        nam:"Dr. Loonawat Wartosin (3ml)",
+        detail:"Ayurvedic composition for elevated or hanging warts",
+        logo:"	https://static.oxinis.com/healthmug/image/asset/498-rm-150.png",
+        price:"80",
+    },
+    {
+        img:"https://static.oxinis.com/healthmug/image/product/5317-2-200.jpg",
+        nam:"Dr. Loonawat Wartosin (3ml)",
+        detail:"Ayurvedic composition for elevated or hanging warts",
+        logo:"	https://static.oxinis.com/healthmug/image/asset/498-rm-150.png",
+        price:"80",
+    },
+    {
+        img:"https://static.oxinis.com/healthmug/image/product/5317-2-200.jpg",
+        nam:"Dr. Loonawat Wartosin (3ml)",
+        detail:"Ayurvedic composition for elevated or hanging warts",
+        logo:"	https://static.oxinis.com/healthmug/image/asset/498-rm-150.png",
+        price:"80",
+    },
+    {
+        img:"https://static.oxinis.com/healthmug/image/product/5317-2-200.jpg",
+        nam:"Dr. Loonawat Wartosin (3ml)",
+        detail:"Ayurvedic composition for elevated or hanging warts",
+        logo:"	https://static.oxinis.com/healthmug/image/asset/498-rm-150.png",
+        price:"80",
+    },
+    {
+        img:"https://static.oxinis.com/healthmug/image/product/5317-2-200.jpg",
+        nam:"Dr. Loonawat Wartosin (3ml)",
+        detail:"Ayurvedic composition for elevated or hanging warts",
+        logo:"	https://static.oxinis.com/healthmug/image/asset/498-rm-150.png",
+        price:"80",
+    },
+    {
+        img:"https://static.oxinis.com/healthmug/image/product/5317-2-200.jpg",
+        nam:"Dr. Loonawat Wartosin (3ml)",
+        detail:"Ayurvedic composition for elevated or hanging warts",
+        logo:"	https://static.oxinis.com/healthmug/image/asset/498-rm-150.png",
+        price:"80",
+    },
+]
+         
+//                 map productData and append in to div bellow
+ 
+        productData.map(function(ele,index){
+          
+            var mainD = document.createElement("div");
+            mainD.setAttribute("class","itm");                   //main div for item img and details
+
+            var d1 = document.createElement("div");               //img
+            var img = document.createElement("img");
+            img.setAttribute("src",ele.img);
+            d1.append(img)
+
+            var d2 = document.createElement("div");
+            var name = document.createElement("p");   
+            name.setAttribute("id","iname")             //name of product
+            name.textContent = ele.nam;
+            var detail = document.createElement("p");
+            detail.textContent = ele.detail;
+            d2.prepend(name,detail)
+            
+            
+            var d3 = document.createElement("div");    // img for review stras img (still ppending)
+            var stardiv1 = document.createElement("i");
+            var stardiv2 = document.createElement("i");
+            var stardiv3 = document.createElement("i");
+            var stardiv4 = document.createElement("i");
+            var stardiv5 = document.createElement("i");
+            stardiv1.setAttribute("class", ele.s1)
+            stardiv2.setAttribute("class", ele.s2)
+            stardiv3.setAttribute("class", ele.s3)
+            stardiv4.setAttribute("class", ele.s4)
+            stardiv5.setAttribute("class", ele.s5)
+            d3.append(stardiv1,stardiv2,stardiv3,stardiv4,stardiv5)
+
+
+            var d4 = document.createElement("div");
+            var logo = document.createElement("img");
+            logo.setAttribute("src",ele.logo);
+            logo.setAttribute("src",ele.logo);
+            d4.append(logo)
+
+            var d5 = document.createElement("div");
+            var price = document.createElement("p");
+            price.setAttribute("id","price")
+            price.textContent = ele.price;
+            var rupeeLogo = document.createElement("i");
+            rupeeLogo.setAttribute("class","fas fa-rupee-sign")
+            d5.append(rupeeLogo,price);
+            
+            var d6 = document.createElement("div");       // add to cart logo 
+            var cartLogo = document.createElement("i");
+            cartLogo.setAttribute("class","fas fa-cart-plus")
+            cartLogo.style.fontSize = "20px"
+            d6.append(cartLogo)
+
+
+          
+              mainD.append(d1,d2,d3,d4,d5,d6)
+           document.querySelector(".items").append(mainD)
+
+
+        })
